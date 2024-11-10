b3dfee82ed532524772f940898559e6839f8a6ef
Fabian Schlegel <fabian.schlegel@grayc.de>
2024-11-10 19:25:00 +0100

    Add OpenMeteo API

---
+++ platformio/include/om_api_response.h
@@ -0,0 +1,104 @@
+//
+// Created by fabian on 22.01.24.
+//
+
+#ifndef PLATFORMIO_OM_API_RESPONSE_H
+#define PLATFORMIO_OM_API_RESPONSE_H
+
+#include <cstdint>
+#include <ArduinoJson.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+
+#define OM_NUM_HOURLY        24
+#define OM_NUM_DAILY          7
+
+#ifdef USE_HTTP
+#define OM_PORT              80
+#else
+#define OM_PORT              443
+#endif
+
+/*
+ * Current weather data API response
+ * https://api.open-meteo.com/v1/dwd-icon?latitude=52.52&longitude=13.41&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,weather_code,cloud_cover,surface_pressure,wind_speed_10m,wind_direction_10m&hourly=temperature_2m
+ */
+typedef struct om_current
+{
+    time_t  dt;                   // epoch seconds
+    float   temperature_2m;       // °C
+    int     relative_humidity_2m; // %
+    float   apparent_temperature; // °C
+    float   precipitation;        // mm
+    int     weather_code;         // wmo code
+    int     cloud_cover;          // %
+    float   surface_pressure;     // hPa
+    float   wind_speed_10m;       // km/h
+    int     wind_direction_10m;   // °
+    bool    is_day;               // true, false
+} om_current_t;
+
+/*
+ * Hourly forecast weather data API response
+ */
+typedef struct om_hourly
+{
+    time_t  dt;                   // epoch seconds
+    float   temperature_2m;       // °C
+    float   precipitation;        // mm
+    int     weather_code;         // wmo code
+} om_hourly_t;
+
+/*
+ * Daily forecast weather data API response
+ * https://api.open-meteo.com/v1/dwd-icon?latitude=52.52&longitude=13.41&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,daylight_duration,sunshine_duration,precipitation_sum,precipitation_probability_max,wind_speed_10m_max,wind_direction_10m_dominant
+ */
+typedef struct om_daily
+{
+    int      weather_code;                // wmo code
+    float    temperature_2m_max;          // °C
+    float    temperature_2m_min;          // °C
+    time_t   sunrise;                     // epoch seconds
+    time_t   sunset;                      // epoch seconds
+    float    daylight_duration;           // s
+    float    sunshine_duration;           // s
+    float    precipitation_sum;           // mm
+    float    precipitation_hours;         // h
+    float    wind_speed_10m_max;          // km/h
+    int      wind_direction_10m_dominant; // °
+} om_daily_t;
+
+/*
+ * National weather alerts data from major national weather warning systems
+ */
+typedef struct om_alerts
+{
+    String  sender_name;      // Name of the alert source.
+    String  event;            // Alert event name
+    int64_t start;            // Date and time of the start of the alert, Unix, UTC
+    int64_t end;              // Date and time of the end of the alert, Unix, UTC
+    String  description;      // Description of the alert
+    String  tags;             // Type of severe weather
+} om_alerts_t;
+
+/*
+ * Response from Open Meto DWD API
+ * https://api.open-meteo.com/v1/dwd-icon?latitude=52.52&longitude=13.41&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,weather_code,cloud_cover,surface_pressure,wind_speed_10m,wind_direction_10m&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,weather_code,surface_pressure,cloud_cover,wind_speed_10m,wind_direction_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,daylight_duration,sunshine_duration,precipitation_sum,precipitation_probability_max,wind_speed_10m_max,wind_direction_10m_dominant
+ */
+typedef struct om_resp
+{
+    om_current_t   current;
+    om_hourly_t    hourly[OM_NUM_HOURLY];
+    om_daily_t     daily[OM_NUM_DAILY];
+} om_resp_t;
+
+DeserializationError deserializeOpenMeteo(WiFiClient &json,
+                                        om_resp_t &r);
+
+#ifdef USE_HTTP
+int getOM(WiFiClient &client, om_resp_t &r);
+#else
+int getOM(WiFiClientSecure &client, om_resp_t &r);
+#endif
+
+#endif //PLATFORMIO_OM_API_RESPONSE_H
---
+++ platformio/src/om_api_response.cpp
@@ -0,0 +1,113 @@
+#include <ArduinoJson.h>
+#include <StreamUtils.h>
+#include "om_api_response.h"
+
+#include <config.h>
+#include <HTTPClient.h>
+
+const String OM_ENDPOINT = "api.open-meteo.com";
+
+#ifdef USE_HTTP
+int getOM(WiFiClient &client, om_resp_t &r)
+#else
+int getOM(WiFiClientSecure &client, om_resp_t &r)
+#endif
+{
+    int attempts = 0;
+    bool rxSuccess = false;
+    DeserializationError jsonErr = {};
+    const String uri = "/v1/dwd-icon?latitude=" + LAT + "&longitude=" + LON
+                       + "&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,weather_code,cloud_cover,surface_pressure,wind_speed_10m,wind_direction_10m&hourly=temperature_2m,precipitation,weather_code,&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset,daylight_duration,sunshine_duration,precipitation_sum,precipitation_hours,wind_speed_10m_max,wind_direction_10m_dominant&forecast_days=5&forecast_hours=24&timezone=Europe%2FBerlin";
+
+    // This string is printed to terminal to help with debugging. The API key is
+    // censored to reduce the risk of users exposing their key.
+    String sanitizedUri = OM_ENDPOINT + uri;
+
+    Serial.print("Attempting HTTP request");
+    Serial.println(": " + sanitizedUri);
+
+    int httpResponse = 0;
+    while (!rxSuccess && attempts < 3)
+    {
+        wl_status_t connection_status = WiFi.status();
+        if (connection_status != WL_CONNECTED)
+        {
+            // -512 offset distinguishes these errors from httpClient errors
+            return -512 - static_cast<int>(connection_status);
+        }
+
+        HTTPClient http;
+        http.setConnectTimeout(HTTP_CLIENT_TCP_TIMEOUT); // default 5000ms
+        http.setTimeout(HTTP_CLIENT_TCP_TIMEOUT); // default 5000ms
+        http.begin(client, OWM_ENDPOINT, OM_PORT, uri);
+        httpResponse = http.GET();
+        if (httpResponse == HTTP_CODE_OK)
+        {
+            jsonErr = deserializeOpenMeteo(http.getStream(), r);
+            if (jsonErr)
+            {
+                // -256 offset distinguishes these errors from httpClient errors
+                httpResponse = -256 - static_cast<int>(jsonErr.code());
+            }
+            rxSuccess = !jsonErr;
+        }
+        client.stop();
+        http.end();
+        Serial.println("  " + String(httpResponse, DEC) + " ");
+        ++attempts;
+    }
+
+    return httpResponse;
+}
+
+DeserializationError deserializeOpenMeteo(WiFiClient &json,
+                                        om_resp_t &r)
+{
+    int i;
+
+    JsonDocument doc;
+    ReadLoggingStream loggingStream(json, Serial);
+    const DeserializationError error = deserializeJson(doc, loggingStream);
+
+    if (error) {
+        return error;
+    }
+
+    const JsonObject current = doc["current"];
+
+    r.current.temperature_2m       = current["temperature_2m"]      .as<float>();
+    r.current.apparent_temperature = current["apparent_temperature"].as<float>();
+    r.current.surface_pressure     = current["surface_pressure"]    .as<float>();
+    r.current.relative_humidity_2m = current["relative_humidity_2m"].as<int>();
+    r.current.cloud_cover          = current["cloud_cover"]         .as<int>();
+    r.current.wind_speed_10m       = current["wind_speed_10m"]      .as<float>();
+    r.current.wind_direction_10m   = current["wind_direction_10m"]  .as<int>();
+    r.current.precipitation        = current["precipitation"]       .as<float>();
+    r.current.weather_code         = current["weather_code"]        .as<int>();
+
+    for (i = 0; i < OM_NUM_HOURLY; i++)
+    {
+        struct tm tm = {0};
+        if (strptime(doc["hourly"]["time"][i].as<const char *>(), "%Y-%m-%dT%H:%M", &tm) != nullptr) {
+            r.hourly[i].dt = mktime(&tm);
+        }
+        r.hourly[i].temperature_2m       = doc["hourly"]["temperature_2m"][i]      .as<float>();
+        r.hourly[i].precipitation        = doc["hourly"]["precipitation"][i]       .as<float>();
+        r.hourly[i].weather_code         = doc["hourly"]["weather_code"][i]        .as<int>();
+    }
+
+    for (i = 0; i < OM_NUM_DAILY; i++)
+    {
+        r.daily[i].temperature_2m_min          = doc["daily"]["temperature_2m_min"][i]         .as<float>();
+        r.daily[i].temperature_2m_max          = doc["daily"]["temperature_2m_max"][i]         .as<float>();
+        r.daily[i].sunshine_duration           = doc["daily"]["sunshine_duration"][i]          .as<float>();
+        r.daily[i].daylight_duration           = doc["daily"]["daylight_duration"][i]          .as<float>();
+        r.daily[i].precipitation_sum           = doc["daily"]["precipitation_sum"][i]          .as<float>();
+        r.daily[i].precipitation_hours         = doc["daily"]["precipitation_hours"][i]        .as<float>();
+        r.daily[i].wind_speed_10m_max          = doc["daily"]["wind_speed_10m_max"][i]         .as<float>();
+        r.daily[i].wind_direction_10m_dominant = doc["daily"]["wind_direction_10m_dominant"][i].as<int>();
+        r.daily[i].weather_code                = doc["daily"]["weather_code"][i]               .as<int>();
+        printf("[%d] SD: %f, DD: %f, PH: %f\n", i, r.daily[i].sunshine_duration, r.daily[i].daylight_duration, r.daily[i].precipitation_hours);
+    }
+    return error;
+} // end deserializeOpenMeteo
